6943bc35dad15253ad6a32c24f4c4d57fe6a7b7b
Lucas Elisei <lucas@elisei.ch>
2023-03-18 23:55:16 +0100

    Dynamically load ingredients textures

--- src/recipes_manager.gd
+++ src/recipes_manager.gd
@@ -5,6 +5,20 @@ class_name RecipesManager extends Node
 
 @export var TIMER_INTERVAL: float = 0.5
 
+var ampoule_texture = preload("res://res/img/ingredients/ampoule.svg")
+var boulon_texture = preload("res://res/img/ingredients/boulon.svg")
+var brocoli_texture = preload("res://res/img/ingredients/brocoli.svg")
+var essence_texture = preload("res://res/img/ingredients/essence.svg")
+var pile_texture = preload("res://res/img/ingredients/pile.svg")
+
+var TEXTURES: Dictionary = {
+	"ampoule": ampoule_texture,
+	"boulon": boulon_texture,
+	"brocoli": brocoli_texture,
+	"essence": essence_texture,
+	"pile": pile_texture
+}
+
 var RECIPES: Array = [
 	Recipe.new(["essence", "pile"]),
 	Recipe.new(["boulon", "brocoli"]),
@@ -42,6 +56,11 @@ func redraw_recipes():
 	var i = 0
 	for recipe in ongoing_recipes:
 		var panel = RecipePanelInstancier.instantiate() as Node2D
+		for j in range(recipe.ingredients.size()):
+			var ingredient = recipe.ingredients[j]
+			var sprite = panel.get_child(0).get_child(j + 1) as Sprite2D
+			sprite.set_texture(TEXTURES[ingredient])
+			sprite.show()
 		panel.position.x = x
 		panel.position.y = y + ((80 + 10) * i)
 		main.add_child(panel)
@@ -62,22 +81,25 @@ func spawn_new_recipe():
 
 func _on_timer_timeout():
 	self.timer_tick += 1
+	if (self.ongoing_recipes.size() >= 5):
+		self.generate_next_spawn()
+		pass
+		
 	if (self.timer_tick >= self.next_spawn_tick):
 		self.spawn_new_recipe()
 
 func is_ongoing_recipe_valid(ingredients: Array):
 	ingredients.sort()
 	
-	self.ongoing_recipes.clear()
+	var same = self.ongoing_recipes.filter(func(recipe): return recipe.ingredients == ingredients)
+	if (same.size() > 0):
+		for recipe in self.ongoing_recipes:
+			if (recipe.ingredients == ingredients):
+				self.ongoing_recipes.erase(recipe)
+				break
+				
 	self.redraw_recipes()
-	
-#	var same = self.ongoing_recipes.filter(func(recipe): return recipe.ingredients == ingredients)
-#	if (same.size() > 0):
-#		for recipe in self.ongoing_recipes:
-#			if (recipe.ingredients == ingredients):
-#				self.ongoing_recipes.erase(recipe)
-#	return (same.size() > 0)
-	return true
+	return (same.size() > 0)
 
 
 class Recipe:
