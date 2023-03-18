48b0ebb4a57fa98fe5f1b39dc17a9cec2ff9725e
David Truan <david@truan.ch>
2023-03-18 20:56:22 +0100

    better bg

--- src/TapisIngredients.gd
+++ src/TapisIngredients.gd
@@ -18,9 +18,9 @@ func _process(delta):
 	$ParallaxBackground.scroll_offset.x += delta*50
 
 func spawn_ingredient():
-	speed_current = speed_current * 1.1
+	speed_current = speed_current * 1.05
 	var ingr = Ingredient.instantiate()
-	ingr.position.x = 0
+	ingr.position.x = $SpawnPosDown.position.x
 	ingr.position.y = randf_range($SpawnPosDown.position.y, $SpawnPosUp.position.y)
 	ingr.speed = speed_current
 	ingr.ingr_name = NAMES.pick_random()
@@ -29,6 +29,6 @@ func spawn_ingredient():
 
 func _on_timer_timeout():
 	spawn_ingredient()
-	spawn_interval -= spawn_interval/30
+	spawn_interval = randf_range(0.5, 1.0)
 	$Timer.start(spawn_interval)
 	
