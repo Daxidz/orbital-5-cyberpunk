b62f912574113332f1fad703b28b42314188cadb
Lucas Elisei <lucas@elisei.ch>
2023-03-19 11:12:39 +0100

    Change score by lives

--- src/score_manager.gd
+++ src/score_manager.gd
@@ -2,25 +2,35 @@ class_name ScoreManager extends Node2D
 
 signal recipe_completed
 signal recipe_timed_out
+signal game_over
 
-var score: int = 0
+var lives: int = 3
+
+@onready var hearts: Array = [
+	$Heart3,
+	$Heart2,
+	$Heart1
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.recipe_completed.connect(self._recipe_completed)
 	self.recipe_timed_out.connect(self._recipe_timed_out)
-	$Label.text = "%d" % score
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if (score != 0):
-		$Label.text = "%+d" % score
-	else:
-		$Label.text = "%d" % score
+	for i in range(self.hearts.size()):
+		var heart = self.hearts[i] as Sprite2D
+		if (self.lives > i):
+			heart.show()
+		else:
+			heart.hide()
 
 func _recipe_completed():
-	self.score += 1
-	
+	pass
+
 func _recipe_timed_out():
-	self.score -= 1
 	$Error.play()
+	self.lives -= 1
+	if (self.lives == 0):
+		self.game_over.emit()
