1a1790a1881bd7b9c55f68176b723f9978924f6a
Lucas Elisei <lucas@elisei.ch>
2023-03-19 03:04:11 +0100

    Better ingredients generation

--- src/TapisIngredients.gd
+++ src/TapisIngredients.gd
@@ -18,6 +18,8 @@ var nb_ingredients_total: int = 0
 @export var ingredients_history_depth: int = 8
 var ingredients_history: Array = []
 
+@onready var recipesManager: RecipesManager = get_tree().get_root().get_node("Main/RecipesManager")
+
 func _ready():
 	spawn_interval = spawn_interval_base
 	$Timer.start(spawn_interval)
@@ -39,9 +41,20 @@ func spawn_ingredient():
 	ingr.position.y = randf_range($SpawnPosDown.position.y, $SpawnPosUp.position.y)
 	ingr.speed = speed_current
 	
-	ingr.ingr_name = NAMES.pick_random()
-	if (self.ingredients_history.find(ingr.ingr_name) != -1):
+	var current_ingredients: Array = [] 
+	if (nb_ingredients_total > 10):
+		for recipe in recipesManager.ongoing_recipes:
+			current_ingredients.append_array(recipe.ingredients)
+		for ingredient in current_ingredients:
+			if (self.ingredients_history.find(ingredient) == -1):
+				ingr.ingr_name = ingredient
+	
+	if (ingr.ingr_name == ""):
 		ingr.ingr_name = NAMES.pick_random()
+	
+		if (self.ingredients_history.find(ingr.ingr_name) != -1):
+			ingr.ingr_name = NAMES.pick_random()
+
 	add_ingredient_to_history(ingr.ingr_name)
 	
 	ingr.set_texture(ingr.ingr_name)
